b4fe3c9c3f85c1f6f3704414103028edd371d84b
Jorge Castillo <Jorgecastillodp@gmail.com>
2018-11-01 18:52:14 -0700

    ehh

---
+++ Main.java
@@ -0,0 +1,28 @@
+// imports 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import java.net.URI;
+
+public class TextAbuddy{
+
+	// account info  final variables
+	public static final String ACCOUNT__SID = "AC...";
+	public static final String AUTH_TOKEN = " tok";
+
+	public static void main(String[] args){
+
+
+	Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
+
+	// set strings to be used 
+	public String[] Message = {"Hey dont smoke it" , " You got it dont forget to stay strong", "Dont give up! it will be over soon."};
+
+		
+	
+
+		
+
+
+}
+
